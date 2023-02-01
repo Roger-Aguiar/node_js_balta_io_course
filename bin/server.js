d220734694ce98d561dc7dc2b7f122893a2a6461
Roger <roger.silvaaguiar.com.br>
2023-02-01 18:06:47 -0300

    It separates the server from the app.

--- bin/server.js
+++ bin/server.js
@@ -1,28 +1,13 @@
 'use strict'
-
+const app = require('../src/app');
+const debug = require('debug')('nodestr:server');
 const http = require('http');
-const debug = require('debug');
-const express = require('express');
-const res = require('express/lib/response');
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
-
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.1',
-        description: 'This is my first route using nodeJs!'
-    });
-});
-
-app.use('/', route);
 server.listen(port);
-
+server.on('listening', onListening);
 console.log("Server is running at port " + port);
 
 function normalizePort(val) {
@@ -35,4 +20,10 @@ function normalizePort(val) {
         return port;
     }
     return false;
+}
+
+function onListening(){
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + address.port;
+    debug('Listening on ' + bind); 
 }
